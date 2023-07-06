07641a4f344e20167bb1f0ce33f8f4a1e577f17d
personal <shaneel_kumar@live.com>
2023-07-06 22:03:17 +1000

    remove container from master layout

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -17,11 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div className="">
-          <Container>{children}</Container>
-        </div>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
