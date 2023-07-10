b33f583df4281059fac4d515a4b439ab5fb731de
personal <shaneel_kumar@live.com>
2023-07-10 16:11:48 +1000

    add album information bar to carousel component

--- client/app/carousel.tsx
+++ client/app/carousel.tsx
@@ -5,7 +5,7 @@ import Container from "../components/container";
 
 export type Album = {
   title: string;
-  year: number;
+  year: number | null;
   image: string;
   listenHere?: string[];
 };
@@ -15,37 +15,75 @@ type Props = {
 };
 
 function Carousel({ albums }: Props) {
-  const [bg, setBg] = useState<string>(albums[0].image || '')
+  const [albumData, setAlbumName] = useState<Album>({
+    title: 'Albums',
+    year: null,
+    image: albums[0].image || '',
+    listenHere: []
+
+  })
 
   return (
     <Container cssExtension="overflow-x-scroll">
       <section
         className="flex justify-center items-center gap-10 relative min-w-full w-fit p-10 mx-auto bg-center bg-cover transition-all"
-        style={{ backgroundImage: bg ? `url(${bg})` : 'none' }}
+        style={{ backgroundImage: albumData.image ? `url(${albumData.image})` : 'none' }}
       >
         <div
           id="bg-overlay"
           className="absolute top-0 left-0 w-full h-full backdrop-grayscale"
         ></div>
         {albums &&
-          albums.map((album) => <AlbumCard key={album.title} album={album} handleBgChange={setBg} />)}
+          albums.map((album) => (
+            <AlbumCard
+              key={album.title}
+              album={album}
+              handleAlbumChange={setAlbumName}
+            />
+          ))}
       </section>
+      <div className="flex justify-between items-center border-black border-b-2">
+        {albumData.title && (
+          <h1 className="text-lg text-center my-4">
+            {albumData.title}
+          </h1>
+        )}
+
+        {albumData.year && (
+          <h1 className="text-lg text-center my-4">
+            {albumData.year}
+          </h1>
+        )}
+
+        {albumData.listenHere && (
+          <ul className="flex gap-8">
+            {albumData.listenHere.map((link, idx) => (
+              <li key={link + idx}>{link}</li>
+            ))}
+          </ul>
+        )}
+      </div>
     </Container>
   );
 }
 
 type AlbumProps = {
   album: Album;
-  handleBgChange: React.Dispatch<React.SetStateAction<string>>;
+  handleAlbumChange: React.Dispatch<React.SetStateAction<Album>>
 };
 
-function AlbumCard({ album, handleBgChange }: AlbumProps) {
+function AlbumCard({ album, handleAlbumChange }: AlbumProps) {
   const { title, year, image, listenHere } = album;
   const [hover, setHover] = useState(false)
 
   function handleMouseEnter() {
     setHover(true)
-    handleBgChange(image)
+    handleAlbumChange({
+      title,
+      year,
+      image,
+      listenHere
+    })
   }
 
   function handleMouseLeave() {
@@ -63,7 +101,6 @@ function AlbumCard({ album, handleBgChange }: AlbumProps) {
         <div className="bg-black bg-opacity-50 w-full h-full text-white flex flex-col justify-center items-center">
           <p>{title}</p>
           <p>{year}</p>
-          {listenHere && <p>listen here</p>}
         </div>
       )}
     </div>
