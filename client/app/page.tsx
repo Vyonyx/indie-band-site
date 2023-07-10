b1ed35e899ceef7d36a261bf83865a1d6b520ef1
personal <shaneel_kumar@live.com>
2023-07-10 14:09:43 +1000

    update album (dummy) data to realistic album cover art

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -11,12 +11,12 @@ export default function Home() {
     {
       title: "Sempiternal",
       year: 2015,
-      image: 'https://images.unsplash.com/photo-1470229722913-7c0e2dbbafd3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+      image: 'https://media.pitchfork.com/photos/5b4e36a4dc6c142e533571c8/1:1/w_450%2Ch_450%2Cc_limit/Mac%2520Miller_Swimming.jpg',
     },
     {
       title: "Heaven and Hell",
       year: 2019,
-      image: 'https://images.unsplash.com/photo-1507676184212-d03ab07a01bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2938&q=80',
+      image: 'https://blog.dozmia.com/content/images/2019/01/Portrait-The-Weeknd.jpg',
     },
   ];
 
